22c891cbb7c31d1f292a4e72e0ba8f9b80031931
sajad shiri <sajad.shiri@live.com>
2026-09-11 20:39:28 +0200

    Add parse_job_entry function to parse job data

--- crawler/parser.py
+++ crawler/parser.py
@@ -3,3 +3,10 @@ All CBRNE-related terminology is stored in `data/keywords/`.
 
 ## Contribution
 Pull requests are welcome.
+def parse_job_entry(raw_entry):
+    return {
+        "title": raw_entry.get("title", ""),
+        "organization": raw_entry.get("org", ""),
+        "location": raw_entry.get("location", ""),
+        "url": raw_entry.get("url", "")
+    }
